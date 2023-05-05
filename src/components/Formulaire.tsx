d690587e9a85880e2514b6f42c2d696bd4cbd9e3
DevIA05 <111973830+DevIA05@users.noreply.github.com>
2023-05-05 12:01:40 +0400

    préparation du layout comportant le formulaire

--- src/components/Formulaire.tsx
+++ src/components/Formulaire.tsx
@@ -3,7 +3,7 @@ import type { Component } from 'solid-js';
 const Formulaire: Component = () => {
     return (
         <div>
-            <form class="grid grid-cols-2 gap-4">
+            {/* <form class="grid grid-cols-2 gap-4">
 
                 <div class="grid grid-cols-10 gap-4">
                     <div class="flex items-center justify-center">
@@ -32,11 +32,11 @@ const Formulaire: Component = () => {
                     </select>
                 </div>
 
-                <div>
-                    <div class="grid grid-cols-10 gap-4">
-                        <div class="flex items-center justify-center">
-                            <label for="marque" class="block font-medium mb-2 w-min" >Marque</label>
-                        </div>
+
+                <div class="grid grid-cols-10 gap-4">
+                    <div class="flex items-center justify-center">
+                        <label for="marque" class="block font-medium mb-2 w-min" >Marque</label>
+
                         <select id="marque" class="select w-full max-w-xs w-min">
                             <option disabled selected>Sélectionnez la marque</option>
                             <option>Homer</option>
@@ -47,25 +47,27 @@ const Formulaire: Component = () => {
                         </select>
                     </div>
                 </div>
-                <div class="grid grid-cols-8 gap-4">
+
+                <div class="grid grid-cols-10 gap-4">
                     <div class="flex items-center justify-center">
-                        <label for="nb-cyl" class="block font-medium mb-2 w-max" >Nombre de cylindre</label>
+                        <label for="nb-cyl" class="block font-medium mb-2 w-min" >Nombre de cylindre</label>
+
+                        <select id="nb-cyl" class="select w-full max-w-xs w-min">
+                            <option disabled selected>Sélectionnez le nombre de cylindre</option>
+                            <option>Homer</option>
+                            <option>Marge</option>
+                            <option>Bart</option>
+                            <option>Lisa</option>
+                            <option>Maggie</option>
+                        </select>
                     </div>
-                    <select id="nb-cyl" class="select w-full max-w-xs w-max">
-                        <option disabled selected>Sélectionnez le nombre de cylindre</option>
-                        <option>Homer</option>
-                        <option>Marge</option>
-                        <option>Bart</option>
-                        <option>Lisa</option>
-                        <option>Maggie</option>
-                    </select>
                 </div>
 
                 <div class="grid grid-cols-10 gap-4">
                     <div class="flex items-center justify-center">
-                        <label for="category" class="block font-medium mb-2 w-max" >Type de carburant</label>
+                        <label for="category" class="block font-medium mb-2 w-min" >Type de carburant</label>
                     </div>
-                    <select id="fuel" class="select w-full max-w-xs w-max">
+                    <select id="fuel" class="select w-full max-w-xs w-min">
                         <option disabled selected>Sélectionnez le type de carburant</option>
                         <option>Homer</option>
                         <option>Marge</option>
@@ -85,9 +87,9 @@ const Formulaire: Component = () => {
 
                 <div class="grid grid-cols-8 gap-4">
                     <div class="flex items-center justify-center">
-                        <label for="eng-vol" class="block font-medium mb-2 w-max" >Volume du moteur</label>
+                        <label for="eng-vol" class="block font-medium mb-2 w-min" >Volume du moteur</label>
                     </div>
-                    <select id="eng-vol" class="select w-full max-w-xs w-max">
+                    <select id="eng-vol" class="select w-full max-w-xs w-min">
                         <option disabled selected>Sélectionnez le volume du moteur</option>
                         <option>Homer</option>
                         <option>Marge</option>
@@ -105,11 +107,11 @@ const Formulaire: Component = () => {
 
                 <div class="grid grid-cols-8 gap-4">
                     <div class="flex items-center justify-center">
-                        <label for="type-bt-vit" class="block font-medium mb-2 w-max" >
+                        <label for="type-bt-vit" class="block font-medium mb-2 w-min" >
                             Boîte de vitesse
                         </label>
                     </div>
-                    <select id="type-bt-vit" class="select w-full max-w-xs w-max">
+                    <select id="type-bt-vit" class="select w-full max-w-xs w-min">
                         <option disabled selected>Sélectionnez le type de boîte de vitesse</option>
                         <option>Homer</option>
                         <option>Marge</option>
@@ -118,11 +120,11 @@ const Formulaire: Component = () => {
 
                 <div class="grid grid-cols-8 gap-4">
                     <div class="flex items-center justify-center">
-                        <label for="type-bt-vit" class="block font-medium mb-2 w-max" >
+                        <label for="type-bt-vit" class="block font-medium mb-2 w-min" >
                             Position du volant
                         </label>
                     </div>
-                    <select id="type-bt-vit" class="select w-full max-w-xs w-max">
+                    <select id="type-bt-vit" class="select w-full max-w-xs w-min">
                         <option disabled selected>Sélectionnez la position du volant</option>
                         <option>à droite</option>
                         <option>à gauche</option>
@@ -131,11 +133,11 @@ const Formulaire: Component = () => {
 
                 <div class="grid grid-cols-8 gap-4">
                     <div class="flex items-center justify-center">
-                        <label for="type-bt-vit" class="block font-medium mb-2 w-max" >
+                        <label for="type-bt-vit" class="block font-medium mb-2 w-min" >
                             Roue motrice
                         </label>
                     </div>
-                    <select id="type-bt-vit" class="select w-full max-w-xs w-max">
+                    <select id="type-bt-vit" class="select w-full max-w-xs w-min">
                         <option disabled selected>Sélectionnez le type de roue motrice</option>
                         <option>Homer</option>
                         <option>Marge</option>
@@ -144,11 +146,11 @@ const Formulaire: Component = () => {
 
                 <div class="grid grid-cols-8 gap-4">
                     <div class="flex items-center justify-center">
-                        <label for="type-bt-vit" class="block font-medium mb-2 w-max" >
+                        <label for="type-bt-vit" class="block font-medium mb-2 w-min" >
                             Nombre de porte
                         </label>
                     </div>
-                    <select id="type-bt-vit" class="select w-full max-w-xs w-max">
+                    <select id="type-bt-vit" class="select w-full max-w-xs w-min">
                         <option disabled selected>Sélectionnez le nombre de porte</option>
                         <option>3</option>
                         <option>5</option>
@@ -157,11 +159,11 @@ const Formulaire: Component = () => {
 
                 <div class="grid grid-cols-8 gap-4">
                     <div class="flex items-center justify-center">
-                        <label for="type-bt-vit" class="block font-medium mb-2 w-max" >
+                        <label for="type-bt-vit" class="block font-medium mb-2 w-min" >
                             Nombre d'airbag
                         </label>
                     </div>
-                    <select id="type-bt-vit" class="select w-full max-w-xs w-max">
+                    <select id="type-bt-vit" class="select w-full max-w-xs w-min">
                         <option disabled selected>Sélectionnez le nombre d'airbag</option>
                         <option>3</option>
                         <option>5</option>
@@ -170,14 +172,14 @@ const Formulaire: Component = () => {
 
                 <div class="grid grid-cols-8 gap-4">
                     <div class="flex items-center justify-center">
-                        <label for="type-bt-vit" class="block font-medium mb-2 w-max" >
+                        <label for="type-bt-vit" class="block font-medium mb-2 w-min" >
                             Nombre de porte
                         </label>
-                        
+
                     </div>
                     <input type="number" placeholder="Km"
-                           
-                           class="input input-bordered input-md w-full max-w-xs" />
+
+                        class="input input-bordered input-md w-full max-w-xs" />
                 </div>
 
                 <div class="col-span-2 flex justify-center">
@@ -186,10 +188,49 @@ const Formulaire: Component = () => {
                     </button>
                 </div>
 
-            </form>
+            </form> */}
 
+            <div class="flex justify-center" >
+
+                {/* ======================================================== */}
+                {/* ==================== Grid Principal ==================== */}
+                {/* ======================================================== */}
+
+                <div class="flex flex-row flex-wrap"
+                    style="background-color:green;
+                        width:1000px; height: 1000px">
+
+                    {/* ================== Colonne 1 ================ */}
+                    <div class="basis-full md:basis-1/2 flex flex-wrap"
+                        style="background-color:red;
+                    width:100px; height: 100px">
+                        <div class="basis-1/2"> 1 </div>
+                        <div class="basis-1/2"> 2 </div>
+                        <div class="basis-1/2"> 3 </div>
+                        {/* <div class=" flex-nowrap"
+                            style="background-color:grey;
+                        width:1000px; height: 1000px">
+
+                        </div> */}
+
+
+                    </div>
+
+                    {/* ================== Colonne 2 ================ */}
+                    <div class="basis-full md:basis-1/2 flex flex-wrap"
+                        style="background-color:yellow;
+                    width:100px; height: 100px">
+
+                        <div class="basis-1/2"> 1 </div>
+                        <div class="basis-1/2"> 2 </div>
+                        <div class="basis-1/2"> 3 </div>
+
+                    </div>
+                </div>
+            </div>
 
         </div>
+
     );
 
 };
