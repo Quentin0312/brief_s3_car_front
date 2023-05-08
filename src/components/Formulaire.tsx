d5ef0816a16ef7c19910c7fdf3734f29b1352325
DevIA05 <111973830+DevIA05@users.noreply.github.com>
2023-05-08 14:53:09 +0400

    simplification du code: génére le code html

--- src/components/Formulaire.tsx
+++ src/components/Formulaire.tsx
@@ -1,195 +1,60 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
+import { JSX } from 'solid-js/jsx-runtime';
+
+interface MyDictionary {
+    [key: string]: {
+        label: string;
+        elem: (string[] | number[]);
+        type: string;
+    };
+}
+
+function createDiv(key: string, dict: MyDictionary): JSX.Element {
+    return (
+        <div class="grid grid-cols-1 md:grid-cols-2 m-10 justify-center items-center">
+        <label for={key}>{dict[key].label}</label>
+        {dict[key].type === "text" ? (
+          <input id={key} type="text" class="input" placeholder={dict[key].elem[0].toString()} />
+        ) : dict[key].type === "checkbox" ? (
+          <input id={key} type="checkbox" class="checkbox" />
+        ) : (
+          <select id={key} class="select">
+            {dict[key].elem.map((item) => (
+              <option>{item}</option>
+            ))}
+          </select>
+        )}
+      </div>
+
+    );
+}
 
 const Formulaire: Component = () => {
+
+    const [data, setData] = createSignal<MyDictionary>({
+        "mrq": { label: "Marque", elem: ["Homer", "Marge", "Bart", "Lisa", "Maggie"], type: "select" },
+        "mdl": { label: "Modèle", elem: ["Homer", "Marge", "Bart", "Lisa", "Maggie"], type: "select" },
+        "nb-portes": { label: "Nombre de portes", elem: [3, 5], type: "select" },
+        "annee": { label: "Année de production", elem: ["année"], type: "text"},
+        "turbo": { label: "Turbo", elem: [""], type: "checkbox"},
+    });
+
+    let itr: number = 0;
+
     return (
         //  ======================= GRID Principal =======================
         <div>
             <div class="flex m-10">
-                <div class="mx-auto grid grid-cols-1 md:grid-cols-2 container h-96 bg-zinc-50">
+                <div class="mx-auto grid grid-cols-1 md:grid-cols-2 container bg-zinc-50">
 
-                    {/* --------------- Colonne 1 --------------- */}
-                    <div class="grid grid-cols-2">
-
-                        <div>
-                            <label for="marque">Marque</label>
-                        </div>
-                        <div>
-                            <select id="marque" class="select">
-                                <option disabled selected>Sélectionnez la marque</option>
-                                <option>Homer</option>
-                                <option>Marge</option>
-                                <option>Bart</option>
-                                <option>Lisa</option>
-                                <option>Maggie</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label for="modele">Modèle</label>
-                        </div>
-                        <div>
-                            <select id="modele" class="select">
-                                <option disabled selected>Sélectionnez le modèle</option>
-                                <option>Homer</option>
-                                <option>Marge</option>
-                                <option>Bart</option>
-                                <option>Lisa</option>
-                                <option>Maggie</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label for="category">Categorie</label>
-                        </div>
-                        <div>
-                            <select id="category" class="select">
-                                <option disabled selected>Sélectionnez la catégorie du véhicule</option>
-                                <option>Homer</option>
-                                <option>Marge</option>
-                                <option>Bart</option>
-                                <option>Lisa</option>
-                                <option>Maggie</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label for="fuel">Type de carburant</label>
-                        </div>
-                        <div>
-                            <select id="fuel" class="select">
-                                <option disabled selected>Sélectionnez le type de carburant</option>
-                                <option>Homer</option>
-                                <option>Marge</option>
-                                <option>Bart</option>
-                                <option>Lisa</option>
-                                <option>Maggie</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label for="type-bt-vit"> Boîte de vitesse </label>
-                        </div>
-                        <div>
-                            <select id="type-bt-vit" class="select">
-                                <option disabled selected>Sélectionnez le type de boîte de vitesse</option>
-                                <option>Homer</option>
-                                <option>Marge</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label for="pos-volant"> Position du volant </label>
-                        </div>
-                        <div>
-                            <select id="pos-volant" class="select">
-                                <option disabled selected>Sélectionnez la position du volant</option>
-                                <option>à droite</option>
-                                <option>à gauche</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label> Kilométrage </label>
-                        </div>
-                        <div>
-                            <input type="text" placeholder="Km"
-                                class="input input-bordered" />
-                        </div>
-
-
-
-                    </div>
-
-                    {/* --------------- Colonne 2 --------------- */}
-                    <div class="grid grid-cols-2">
-
-                        <div>
-                            <label for="nb-cyl">Nombre de cylindre</label>
-                        </div>
-                        <div>
-                            <select id="nb-cyl" class="select">
-                                <option disabled selected>Sélectionnez le nombre de cylindre</option>
-                                <option>Homer</option>
-                                <option>Marge</option>
-                                <option>Bart</option>
-                                <option>Lisa</option>
-                                <option>Maggie</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label for="cuir">Intérieur cuir</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" checked={true} class="checkbox" />
-                        </div>
-
-                        <div>
-                            <label for="eng-vol">Volume du moteur</label>
-                        </div>
-
-                        <div>
-                            <select id="eng-vol" class="select">
-                                <option disabled selected>Sélectionnez le volume du moteur</option>
-                                <option>Homer</option>
-                                <option>Marge</option>
-                                <option>Bart</option>
-                                <option>Lisa</option>
-                                <option>Maggie</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label for="turbo">Turbo</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" checked={true} class="checkbox" />
-                        </div>
-
-                        <div>
-                            <label for="roue-motrice"> Roue motrice </label>
-                        </div>
-                        <div>
-                            <select id="roue-motrice" class="select">
-                                <option disabled selected>Sélectionnez le type de roue motrice</option>
-                                <option>Homer</option>
-                                <option>Marge</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label for="nb-portes"> Nombre de portes </label>
-                        </div>
-                        <div>
-                            <select id="nb-portes" class="select">
-                                <option disabled selected>Sélectionnez le nombre de porte</option>
-                                <option>3</option>
-                                <option>5</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label for="nb-airbag"> Nombre d'airbag </label>
-                        </div>
-                        <div>
-                            <select id="nb-airbag" class="select">
-                                <option disabled selected>Sélectionnez le nombre d'airbag</option>
-                                <option>3</option>
-                                <option>5</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label> Année de production </label>
-                        </div>
-                        <div>
-                            <input type="text" placeholder="année"
-                                class="input input-bordered"
-                                />
-                        </div>
-
-                    </div>
-
+                    {Object.keys(data()).map((key) => {
+                        itr++;
+                        if (itr % 2 === 0) {
+                            return (createDiv(key, data()));
+                        } else {
+                            return (createDiv(key, data()));
+                        }
+                    })}
 
 
                 </div>
